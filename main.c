7a3bcc0bc18d385f032955afbe8707f4b46ca1bb
ClakySecomd <89134670+ClakySecomd@users.noreply.github.com>
2022-05-30 12:24:41 +0200

    Implemented method doubleinfield

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ void setconfig(field *sudoku)
     for(int i = 0;i < 9;i++)
     {
         for(int j = 0;j < 9;j++){
-            if(sudoku[i].el[j] != 0)
+            if( sudoku[i].el[j] != 0 )
             {
                 sudoku[i].el[j] = -sudoku[i].el[j];
             }
@@ -22,25 +22,94 @@ void setconfig(field *sudoku)
     }
 }
 
+int doubleinfield(field x)
+{
+    int br[10];
+    
+    for(int j = 0;j < 9;j++)
+    {
+        br[ abs(x.el[j]) ]++;
+    }
+
+    // make this into a counter method
+    // and always pass it an array of elements
+    // which needs to be checked for duplicates
+
+    if(br[1] != 1 || br[2] != 1 || br[3] != 1 || br[4] != 1|| br[5] != 1
+     || br[6] != 1 || br[7] != 1 || br[8] != 1 || br[9] != 1)
+       return 1;
+
+    return 0;
+}
+
+int doubleincolumn(field *sudoku, int i)
+{
+    int j, k;
+    if( i < 3 )
+    {
+        j = i + 3; k = i + 6;
+    }
+
+    else if( i < 6 )
+    {
+        j = i - 3; k = i + 3;
+    }
+
+    else
+    {
+        j = i - 6; k = i - 3;
+    }
+    
+
+    // we need a counter method
+}
+
+int interference(field *sudoku)
+{
+    for(int i = 0;i < 9;i++)
+    {
+        if( doubleinfield(sudoku[i]) )
+            return 0;
+        
+
+        if( doubleincolumn(sudoku, i) )
+            return 0;
+
+        if( doubleinrow(sudoku, i) )
+            return 0;
+
+    }
+
+    return 1;
+}
+
 int stepup(field *sudoku, int i, int j)
 {
-    if(i == 9)
-        return 1;
+    if( sudoku[i].el[j] == 10 )
+        return 0;
     //this is the end
 
     do
     {
         if( sudoku[i].el[j] >= 0 )
-        {
-            
-        }
-    } while (nointerference(sudoku));
+            sudoku[i].el[j]++;
+        
+
+        if( sudoku[i].el[j] == 10 )
+        return 0;
+    } while(interference(sudoku) );
     
+    // the goal here is to try all possibilities
+    // by increasing the number on which we are
+    // and then letting the program continue
+    // with other numbers
+
+    return 1;
 }
 
 int main(int argc, char *args[])
 {
-    if(argc == 2)
+    if( argc == 2 )
     {
         // in this case we have two file names
         // one file contains the puzzle
@@ -70,7 +139,7 @@ int main(int argc, char *args[])
 
                 j += 3;
 
-                if(j == 9)
+                if( j == 9 )
                 {
                     i += 1;
                     j = 0;
@@ -86,10 +155,10 @@ int main(int argc, char *args[])
             i = 0;  j = 0;
             do
             {
-                int test = stepup(sudoku, i, j);
-                if(test)
+                
+                if( stepup(sudoku, i, j) )
                 {
-                    if(completeconfig(sudoku, i, j))
+                    if( completeconfig(sudoku, i, j) )
                     {
                         process(sudoku, w);
                         break;
@@ -103,7 +172,7 @@ int main(int argc, char *args[])
             
         }
     }
-    else if(argc == 1)
+    else if( argc == 1 )
     {
         // in this case we have a one file
         // this file will be used to
